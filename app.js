b5dfe52c8a80e39094f4a8a1495216d664d04045
TheMLevin <76596757+TheMLevin@users.noreply.github.com>
2020-12-25 19:10:32 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hi Elisa what\'s up!'));
+app.get('/', (req, res) => res.send('Good Day Sir!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
